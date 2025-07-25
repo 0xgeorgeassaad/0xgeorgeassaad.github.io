7c85811c800d4a3536ed56308e6f91c6ab8cb295
0xgeorgeassaad <lopgogo@gmail.com>
2025-07-25 01:13:55 +0000

    Deploying to gh-pages from @ 0xgeorgeassaad/0xgeorgeassaad.github.io@8f0d9b9bdab4d5b7b199f2f404732603c01ffb9f 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,51 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-home",
+    title: "Home",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "news-i-graduated-summa-cum-laude-with-a-bachelor-s-degree-in-communications-and-information-engineering-from-zewail-city-of-science-and-technology",
+          title: 'I graduated summa cum laude with a bachelor’s degree in Communications and Information...',
+          description: "",
+          section: "News",},{id: "news-i-graduated-with-distinction-from-the-ai-for-science-master-s-program-at-aims-south-africa-funded-by-google-deepmind",
+          title: 'I graduated with distinction from the AI for science master’s program at AIMS...',
+          description: "",
+          section: "News",},{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
